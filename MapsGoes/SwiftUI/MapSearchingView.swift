8c554585280aee946a3619871e090256de581524
dangar11 <tkach.igor.vladimirovich@gmail.com>
2020-01-16 22:30:49 +0200

    Reactive @State variable, perform searching places and annotates them at map

--- MapsGoes/SwiftUI/MapSearchingView.swift
+++ MapsGoes/SwiftUI/MapSearchingView.swift
@@ -9,7 +9,11 @@
 import SwiftUI
 import MapKit
 
+
+
 struct MapViewContainer: UIViewRepresentable {
+  
+  var annotations = [MKPointAnnotation]()
   
   let mapView = MKMapView()
   
@@ -20,15 +24,18 @@ struct MapViewContainer: UIViewRepresentable {
   }
   
   fileprivate func setupRegionForMap() {
-    let centerCoordinate = CLLocationCoordinate2D(latitude: 49.420382, longitude: 26.988605)
-    let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    let centerCoordinate = CLLocationCoordinate2D(latitude: 50.439833, longitude: 30.540917)
+    let span = MKCoordinateSpan(latitudeDelta: 0.3, longitudeDelta: 0.3)
     let region = MKCoordinateRegion(center: centerCoordinate, span: span)
     mapView.setRegion(region, animated: true)
     
   }
   
   func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapViewContainer>) {
-    
+    uiView.removeAnnotations(uiView.annotations)
+    uiView.addAnnotations(annotations)
+    uiView.showAnnotations(uiView.annotations, animated: true)
+
   }
   
   typealias UIViewType = MKMapView
@@ -38,19 +45,26 @@ struct MapViewContainer: UIViewRepresentable {
 
 
 struct MapSearchingView: View {
+  
+  @State var searchQuery = "sushi"
+  
+  @State var annotations = [MKPointAnnotation]()
+  
     var body: some View {
       ZStack(alignment: .top) {
-        MapViewContainer()
+        MapViewContainer(annotations: annotations)
           .edgesIgnoringSafeArea(.all)
         HStack {
           Button(action: {
+            //let's perform airport search
+            self.performSearch(query: self.searchQuery)
           }, label: {
-            Text("Search for airports")
+            Text("Search for \(searchQuery)")
             .padding()
               .background(Color.white)
           })
           Button(action: {
-            
+            self.annotations = []
           }, label: {
             Text("Clear Annotation")
             .padding()
@@ -60,11 +74,37 @@ struct MapSearchingView: View {
         .shadow(radius: 3)
         
       }
+  }
+      
+      fileprivate func performSearch(query: String) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = query
+        let localSearch = MKLocalSearch(request: request)
+        localSearch.start { (response, error) in
+          //Error
+          if let error = error {
+            print("Search query failed: ", error.localizedDescription)
+          }
+          
+          var airportAnnotations = [MKPointAnnotation]()
+          
+          //Success
+          response?.mapItems.forEach({ (mapItem) in
+            let annotation = MKPointAnnotation()
+            annotation.title = mapItem.name
+            annotation.coordinate = mapItem.placemark.coordinate
+            airportAnnotations.append(annotation)
+          })
+          self.annotations = airportAnnotations
+      }
     }
-}
+
+
 
 struct MapSearchingView_Previews: PreviewProvider {
     static var previews: some View {
         MapSearchingView()
     }
+}
+
 }
