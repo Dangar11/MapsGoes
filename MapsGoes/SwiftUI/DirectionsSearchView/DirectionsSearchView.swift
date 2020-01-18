979a1e64d7f60db263f257fe7fd90fa3e8554f17
dangar11 <tkach.igor.vladimirovich@gmail.com>
2020-01-18 17:35:24 +0200

    NavigationView and NavigationLink with variable bindings

--- MapsGoes/SwiftUI/DirectionsSearchView/DirectionsSearchView.swift
+++ MapsGoes/SwiftUI/DirectionsSearchView/DirectionsSearchView.swift
@@ -8,11 +8,11 @@
 
 import SwiftUI
 import MapKit
+import Combine
 
 struct DirectionMapView: UIViewRepresentable {
   
   typealias UIViewType = MKMapView
-  
   
   
   func makeUIView(context: UIViewRepresentableContext<DirectionMapView>) -> MKMapView {
@@ -25,61 +25,92 @@ struct DirectionMapView: UIViewRepresentable {
 }
 
 
+struct SelectLocationView: View {
+    
+    // here is the magic
+    @Binding var isShowing: Bool
+    
+    var body: some View {
+        VStack {
+            Button(action: {
+                
+                // need to dismiss this view somehow???
+                self.isShowing = false
+                
+            }, label: {
+                Text("Dismiss")
+            })
+        }
+        .navigationBarTitle("")
+        .navigationBarHidden(true)
+    }
+}
+
+
+
 struct DirectionsSearchView: View {
   
-  let appWidth = UIApplication.shared.windows.filter({$0.isKeyWindow}).first?.frame.width
-  let safeAreaHeight = UIApplication.shared.windows.filter({$0.isKeyWindow}).first?.safeAreaInsets.top
+  
+  @State var isSelectingSource = false
   
   var body: some View {
-    ZStack(alignment: .top) {
-      
-      VStack(spacing: 0) {
-        VStack {
-          HStack(spacing: 16) {
-            Image(uiImage: #imageLiteral(resourceName: "start").withRenderingMode(.alwaysTemplate)).resizable()
-              .frame(width: 32, height: 32)
-              .foregroundColor(.white)
-            HStack {
-              Text("Source")
-              Spacer()
+    
+    NavigationView {
+      ZStack(alignment: .top) {
+        VStack(spacing: 0) {
+          VStack {
+            HStack(spacing: 16) {
+              Image(uiImage: #imageLiteral(resourceName: "start").withRenderingMode(.alwaysTemplate)).resizable()
+                .frame(width: 32, height: 32)
+                .foregroundColor(.white)
+              NavigationLink(destination: SelectLocationView(isShowing: $isSelectingSource), isActive: $isSelectingSource) {
+                HStack {
+                  Text("Source")
+                  Spacer()
+                }
+                .padding()
+                .background(Color.white)
+                .cornerRadius(5)
+              }
             }
-            .padding()
-            .background(Color.white)
-            .cornerRadius(5)
-            
-          }
-          HStack(spacing: 16) {
-            Image(uiImage: #imageLiteral(resourceName: "end").withRenderingMode(.alwaysTemplate)).resizable()
-              .frame(width: 32, height: 32)
-              .foregroundColor(.white)
-            HStack {
-              Text("Destination")
-              Spacer()
+            HStack(spacing: 16) {
+              Image(uiImage: #imageLiteral(resourceName: "end").withRenderingMode(.alwaysTemplate)).resizable()
+                .frame(width: 32, height: 32)
+                .foregroundColor(.white)
+              HStack {
+                Text("Destination")
+                Spacer()
+              }
+              .padding()
+              .background(Color.white)
+              .cornerRadius(5)
             }
-            .padding()
-            .background(Color.white)
-            .cornerRadius(5)
           }
+          .padding()
+          .background(Color.blue)
+          
+          DirectionMapView()
+            .edgesIgnoringSafeArea(.bottom)
         }
-        .padding()
-        .background(Color.blue)
         
-        DirectionMapView()
-          .edgesIgnoringSafeArea(.bottom)
+        //status bar cover up
+        
+        Spacer().frame(width: UIApplication.shared.windows.filter{$0.isKeyWindow}.first?.frame.width,
+                       height: UIApplication.shared.windows.filter{$0.isKeyWindow}.first?.safeAreaInsets.top)
+          .background(Color.blue)
+          .edgesIgnoringSafeArea(.top)
+        
       }
-      
-      //status bar cover up
-
-      Spacer().frame(width: appWidth, height: safeAreaHeight)
-        .background(Color.blue)
-        .edgesIgnoringSafeArea(.top)
-      
+    .navigationBarTitle("Directions")
+    .navigationBarHidden(true)
     }
   }
 }
 
+
+
 struct DirectionsSearchView_Previews: PreviewProvider {
-    static var previews: some View {
-        DirectionsSearchView()
-    }
+  static var previews: some View {
+    DirectionsSearchView()
+  }
 }
