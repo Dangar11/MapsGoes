588d14a7b8c46183b888090786beee7bf523e2e6
dangar11 <tkach.igor.vladimirovich@gmail.com>
2020-01-20 16:53:15 +0200

    Building search screen with swiftUI and connect that with TextField+combine framework .onReceive .publisher .debounce

--- MapsGoes/SwiftUI/DirectionsSearchView/DirectionsSearchView.swift
+++ MapsGoes/SwiftUI/DirectionsSearchView/DirectionsSearchView.swift
@@ -26,46 +26,101 @@ struct DirectionMapView: UIViewRepresentable {
 
 
 struct SelectLocationView: View {
-    
-    // here is the magic
-    @Binding var isShowing: Bool
-    
-    var body: some View {
-        VStack {
-            Button(action: {
-                
-                // need to dismiss this view somehow???
-                self.isShowing = false
-                
-            }, label: {
-                Text("Dismiss")
-            })
-        }
-        .navigationBarTitle("")
-        .navigationBarHidden(true)
-    }
-}
-
-
-
-struct DirectionsSearchView: View {
   
+  // here is the magic
+  @Binding var isShowing: Bool
   
-  @State var isSelectingSource = false
+  @State var mapItems = [MKMapItem]()
+  
+  @State var searchQuery = ""
   
   var body: some View {
+    VStack {
+      HStack(spacing: 16) {
+        Button(action: {
+          
+          // need to dismiss this view somehow???
+          self.isShowing = false
+          
+        }, label: {
+          Image(uiImage: #imageLiteral(resourceName: "back").withRenderingMode(.alwaysTemplate)).resizable()
+            .foregroundColor(.black)
+            .frame(width: 32, height: 32)
+        })
+        
+        TextField("Enter search term", text: $searchQuery)
+          .onReceive(NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification).debounce(for: .milliseconds(500), scheduler: RunLoop.main)) {_ in
+            //search
+            let request = MKLocalSearch.Request()
+            request.naturalLanguageQuery = self.searchQuery
+            let search = MKLocalSearch(request: request)
+            search.start { (response, error) in
+              //Error
+              if let error = error {
+                print("Unable to search: ", error)
+              }
+              guard let mapItemsResponse = response?.mapItems else { return }
+              self.mapItems = mapItemsResponse
+            }
+        }
+        }.padding()
+        
+        if mapItems.count > 0 {
+          ScrollView {
+            ForEach(mapItems, id: \.self) { item in
+              HStack {
+                VStack(alignment: .leading) {
+                  Text("\(item.name ?? "")")
+                    .font(.headline)
+                  Text("\(item.address())")
+                }
+                Spacer()
+              }
+              .padding()
+            }
+          }
+        }
+        Spacer()
+      }
+      .edgesIgnoringSafeArea(.bottom)
+      .navigationBarTitle("")
+      .navigationBarHidden(true)
+    }
+  }
+
+  
+  
+  struct DirectionsSearchView: View {
     
-    NavigationView {
-      ZStack(alignment: .top) {
-        VStack(spacing: 0) {
-          VStack {
-            HStack(spacing: 16) {
-              Image(uiImage: #imageLiteral(resourceName: "start").withRenderingMode(.alwaysTemplate)).resizable()
-                .frame(width: 32, height: 32)
-                .foregroundColor(.white)
-              NavigationLink(destination: SelectLocationView(isShowing: $isSelectingSource), isActive: $isSelectingSource) {
+    
+    @State var isSelectingSource = false
+    
+    var body: some View {
+      
+      NavigationView {
+        ZStack(alignment: .top) {
+          VStack(spacing: 0) {
+            VStack {
+              HStack(spacing: 16) {
+                Image(uiImage: #imageLiteral(resourceName: "start").withRenderingMode(.alwaysTemplate)).resizable()
+                  .frame(width: 32, height: 32)
+                  .foregroundColor(.white)
+                NavigationLink(destination: SelectLocationView(isShowing: $isSelectingSource), isActive: $isSelectingSource) {
+                  HStack {
+                    Text("Source")
+                    Spacer()
+                  }
+                  .padding()
+                  .background(Color.white)
+                  .cornerRadius(5)
+                }
+              }
+              HStack(spacing: 16) {
+                Image(uiImage: #imageLiteral(resourceName: "end").withRenderingMode(.alwaysTemplate)).resizable()
+                  .frame(width: 32, height: 32)
+                  .foregroundColor(.white)
                 HStack {
-                  Text("Source")
+                  Text("Destination")
                   Spacer()
                 }
                 .padding()
@@ -73,44 +128,32 @@ struct DirectionsSearchView: View {
                 .cornerRadius(5)
               }
             }
-            HStack(spacing: 16) {
-              Image(uiImage: #imageLiteral(resourceName: "end").withRenderingMode(.alwaysTemplate)).resizable()
-                .frame(width: 32, height: 32)
-                .foregroundColor(.white)
-              HStack {
-                Text("Destination")
-                Spacer()
-              }
-              .padding()
-              .background(Color.white)
-              .cornerRadius(5)
-            }
+            .padding()
+            .background(Color.blue)
+            
+            DirectionMapView()
+              .edgesIgnoringSafeArea(.bottom)
           }
-          .padding()
-          .background(Color.blue)
           
-          DirectionMapView()
-            .edgesIgnoringSafeArea(.bottom)
+          //status bar cover up
+          
+          Spacer().frame(width: UIApplication.shared.windows.filter{$0.isKeyWindow}.first?.frame.width,
+                         height: UIApplication.shared.windows.filter{$0.isKeyWindow}.first?.safeAreaInsets.top)
+            .background(Color.blue)
+            .edgesIgnoringSafeArea(.top)
+          
         }
-        
-        //status bar cover up
-        
-        Spacer().frame(width: UIApplication.shared.windows.filter{$0.isKeyWindow}.first?.frame.width,
-                       height: UIApplication.shared.windows.filter{$0.isKeyWindow}.first?.safeAreaInsets.top)
-          .background(Color.blue)
-          .edgesIgnoringSafeArea(.top)
-        
+        .navigationBarTitle("Directions")
+        .navigationBarHidden(true)
       }
-    .navigationBarTitle("Directions")
-    .navigationBarHidden(true)
     }
   }
+  
+  
+  
+  struct DirectionsSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+      DirectionsSearchView()
+    }
 }
 
-
-
-struct DirectionsSearchView_Previews: PreviewProvider {
-  static var previews: some View {
-    DirectionsSearchView()
-  }
-}
